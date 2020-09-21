ddbc7096eb286ade57c85efc4fdf0bb8d14d2fa0
Muhanad Al-Rubaiee <muhanad40@gmail.com>
2020-09-21 14:28:34 +0100

    Allow user to specify the number of items to check the highest product

--- index.js
+++ index.js
@@ -12,14 +12,14 @@ function multiplyItems(arr) {
   return arr.reduce((acc, number) => acc * number, 1);
 }
 
-function productOfTopThree(arr = []) {
+function productOfTopN(arr = [], numberOfMultiples) {
   // Strip invalid/falsy entries
   const numbers = arr.filter(n => typeof n === 'number' && !isNaN(n));
 
-  if (!numbers.length || numbers.length < 3) return 0;
+  if (!numbers.length || !numberOfMultiples || numbers.length < numberOfMultiples) return 0;
 
-  const largestThreeNumbers = sortArr(numbers, 'desc').slice(0, 3);
-  const lowestTwoNumbers = sortArr(numbers, 'asc').slice(0, 2);
+  const largestThreeNumbers = sortArr(numbers, 'desc').slice(0, numberOfMultiples);
+  const lowestTwoNumbers = sortArr(numbers, 'asc').slice(0, numberOfMultiples - 1);
 
   return Math.max(
     multiplyItems(largestThreeNumbers),
@@ -27,4 +27,4 @@ function productOfTopThree(arr = []) {
   );
 }
 
-module.exports = productOfTopThree;
+module.exports = productOfTopN;
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -11,25 +11,29 @@ describe('productOfTopThree', () => {
 		});
 
 		it('should return 0 when given an array of less than three numbers', () => {
-			expect(productOfTopThree([1, 5])).toEqual(0);
+			expect(productOfTopThree([1, 5], 3)).toEqual(0);
 		});
 	});
 
 	describe('Valid cases', () => {
+		it('should return the product of the highest four numbers when given only positive numbers', () => {
+			expect(productOfTopThree([1, 5, 3, 7, 3, 2], 4)).toEqual(315);
+		});
+
 		it('should return the product of the highest three numbers when given only positive numbers', () => {
-			expect(productOfTopThree([1, 5, 3, 7, 3, 2])).toEqual(105);
+			expect(productOfTopThree([1, 5, 3, 7, 3, 2], 3)).toEqual(105);
 		});
 
 		it('should return the product of the highest three numbers when given one negative number and multiple positive numbers', () => {
-			expect(productOfTopThree([1, 5, 3, 7, 3, -5, 2])).toEqual(105);
+			expect(productOfTopThree([1, 5, 3, 7, 3, -5, 2], 3)).toEqual(105);
 		});
 
 		it('should return the product of the highest three numbers when given two negative numbers and multiple positive numbers', () => {
-			expect(productOfTopThree([1, 5, 3, 7, 3, -5, -7, 2])).toEqual(245);
+			expect(productOfTopThree([1, 5, 3, 7, 3, -5, -7, 2], 3)).toEqual(245);
 		});
 
 		it('should return the product of the highest three numbers when also given falsy values', () => {
-			expect(productOfTopThree([1, 5, 3, 7, 3, -5, -7, 2, NaN, null])).toEqual(245);
+			expect(productOfTopThree([1, 5, 3, 7, 3, -5, -7, 2, NaN, null], 3)).toEqual(245);
 		});
 	});
 });
