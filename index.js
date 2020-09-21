00b84c99e70ea558857e56436dd2dfa36fc2ede6
Muhanad Al-Rubaiee <muhanad40@gmail.com>
2020-09-21 14:17:09 +0100

    Improve solution

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
+function sortArr(arr, direction) {
+	return arr.sort((n1, n2) => {
+		if (direction === 'desc') {
+			return (n1 > n2) ? -1 : 1;
+		} else if (direction === 'asc') {
+			return (n1 > n2) ? 1 : -1;
+		}
+	});
+}
+
+function multiplyItems(arr) {
+	return arr.reduce((acc, number) => acc * number, 1);
+}
+
 function productOfTopThree(arr = []) {
-	// Strip invalid numbers like NaN or negative numbers
-	const numbers = arr.filter(n => Math.sign(n) === 1);
+	// Strip invalid/falsy entries
+	const numbers = arr.filter(n => typeof n === 'number' && !isNaN(n));
 
-	if (!numbers.length) return 0;
+	if (!numbers.length || numbers.length < 3) return 0;
 
-	return numbers.sort((n1, n2) => (n1 > n2) ? -1 : 1)
-		.slice(0, 3)
-		.reduce((acc, number) => acc * number, 1);
+	const largestThreeNumbers = sortArr(numbers, 'desc').slice(0, 3);
+	const lowestTwoNumbers = sortArr(numbers, 'asc').slice(0, 2);
+
+	return Math.max(
+		multiplyItems(largestThreeNumbers),
+		multiplyItems(lowestTwoNumbers) * largestThreeNumbers[0]
+	);
 }
 
 module.exports = productOfTopThree;
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,27 +1,35 @@
 const productOfTopThree =  require('../');
 
 describe('productOfTopThree', () => {
-	it('should return 0 if given empty array', () => {
-		expect(productOfTopThree([])).toEqual(0);
+	describe('Invalid cases', () => {
+		it('should return 0 if given empty array', () => {
+			expect(productOfTopThree([])).toEqual(0);
+		});
+
+		it('should return 0 if given array of falsy items', () => {
+			expect(productOfTopThree([null, false, undefined, NaN])).toEqual(0);
+		});
+
+		it('should return 0 when given an array of less than three numbers', () => {
+			expect(productOfTopThree([1, 5])).toEqual(0);
+		});
 	});
 
-	it('should return 0 if given NaN', () => {
-		expect(productOfTopThree([NaN])).toEqual(0);
-	});
+	describe('Valid cases', () => {
+		it('should return the product of the highest three numbers when given only positive numbers', () => {
+			expect(productOfTopThree([1, 5, 3, 7, 3, 2])).toEqual(105);
+		});
 
-	it('should return 0 if given array of falsy items', () => {
-		expect(productOfTopThree([null, false, undefined])).toEqual(0);
-	});
+		it('should return the product of the highest three numbers when given one negative number and multiple positive numbers', () => {
+			expect(productOfTopThree([1, 5, 3, 7, 3, -5, 2])).toEqual(105);
+		});
 
-	it('should return the product of the highest three numbers when given only positive numbers', () => {
-		expect(productOfTopThree([1, 5, 3, 7, 3, 2])).toEqual(105);
-	});
+		it('should return the product of the highest three numbers when given at least two negative numbers and multiple positive numbers', () => {
+			expect(productOfTopThree([1, 5, 3, 7, 3, -5, -7, 2])).toEqual(245);
+		});
 
-	it('should return the product of the highest three numbers when given positive and negative numbers', () => {
-		expect(productOfTopThree([1, 5, 3, 7, 3, -5, -2, 2])).toEqual(105);
-	});
-
-	it('should return the product of the highest three numbers when also given NaN', () => {
-		expect(productOfTopThree([1, 5, 3, 7, 3, NaN, -5, -2, 2])).toEqual(105);
+		it('should return the product of the highest three numbers when also given falsy values', () => {
+			expect(productOfTopThree([1, 5, 3, 7, 3, -5, -7, 2, NaN, null])).toEqual(245);
+		});
 	});
 });
